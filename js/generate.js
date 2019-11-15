ecb298c9ecf89a323dae68f87275c988305ed877
Néstor Ojeda González <nestorojedagonzalez@gmail.com>
2019-11-15 20:24:07 +0000

    cambios menores y corrección de errores

--- js/generate.js
+++ js/generate.js
@@ -21,10 +21,10 @@ function generateWebReference() {
 
     document.getElementsByName("outputReference").innerHTML = "hola";
 
-    var author = document.forms["apaFormBook"]["authorInput"].value;
-    var title = document.forms["apaFormBook"]["titleInput"].value;
-    var date = document.forms["apaFormBook"]["dateInput"].value;
-    var url = document.forms["apaFormBook"]["urlInput"].value;
+    var author = document.forms["apaFormWeb"]["authorInput"].value;
+    var title = document.forms["apaFormWeb"]["titleInput"].value;
+    var date = document.forms["apaFormWeb"]["dateInput"].value;
+    var url = document.forms["apaFormWeb"]["urlInput"].value;
 
     var firstName = author.split(' ').slice(0, -1).join(' ');
     var lastName = author.split(' ').slice(-1).join(' ');
